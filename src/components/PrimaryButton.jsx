e53a5fd3398c3a0e9124437ffc0f0e1c382f75ee
Godspower Similoluwa <131388888+gpsimi@users.noreply.github.com>
2023-10-17 22:52:29 +0100

    Hero update

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames'
 
 const PrimaryButton = ({children, href, className, onClick, ...props}) => {
     const cx = classNames ([
-        "text-primary bg-buttonColor py-4 px-8 rounded-xl font-bold border-white border hover:bg-transparent hover:text-white duration-700 max-lg:",
+        "text-primary bg-buttonColor py-3 px-5 rounded-xl font-bold border-white border hover:bg-transparent hover:text-white duration-700 max-lg:",
         className
     ]);
     return (
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import PrimaryButton from "../components/PrimaryButton";
-import { BsArrowRightShort } from "react-icons/bs"
+import { BsArrowRightShort } from "react-icons/bs";
+// import Excitement from "../assets/images/excitement-1.png";
+// import TestifierImage1 from "../assets/images/testifier-2.png";
+import DashImage from "../assets/images/DashImage.jpg";
 
 const Hero = () => {
   return (
@@ -8,7 +11,7 @@ const Hero = () => {
       id="#home"
       className="w-full flex flex-col min-h-screen max-container gap-10"
     >
-      <div className="relative flex flex-col items-center justify-center pt-40 max-xl:padding-x lg:pt-[230px]">
+      <div className="relative flex flex-col items-center justify-center pt-40 max-xl:padding-x lg:pt-[230px]">       
         <h1 className='text-main text-center text-6xl max-sm:text-[17px] max-sm:leading-[23px] font-semibold max-xl:text-4xl'>
           Experiencing the Magic Sound <br /> and Storytelling
         </h1>
@@ -16,10 +19,36 @@ const Hero = () => {
           Explore vibrant soundscapes where stories of every kind come alive,
           taking <br /> you on an immersive journey through captivating narratives.
         </p>
-        <PrimaryButton className={"max-sm:mt-6 mt-10 flex items-center"}>
-          Get Started Free
-          <BsArrowRightShort className='ml-1 text-3xl'/>
-        </PrimaryButton>
+
+        {/* <div className="absolute -top-[-10%] -right-[16%]">
+          <img 
+            src={Excitement} 
+            alt=""
+            className="w-[120px]"
+          />
+        </div> */}
+
+        <div className='mt-6'>
+          <PrimaryButton className={"max-sm:mt-1 mt-10 flex items-center"}>
+            Get Started Free
+            <BsArrowRightShort className='ml-1 text-3xl'/>
+          </PrimaryButton>
+        </div>
+
+        
+        
+        <div className="mt-16 max-sm:mt-8 p-x-4 flex items-center">
+        {/* <img 
+            src={TestifierImage1} 
+            alt=""
+            className="bg-white"
+          /> */}
+          <img 
+            src={DashImage} 
+            alt="Dashboard-Image"
+            className="box-shadow rounded-3xl max-sm:rounded-lg w-full object-contain"
+          />
+        </div>
       </div>
     </section>
   )

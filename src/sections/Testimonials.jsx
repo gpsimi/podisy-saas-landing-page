9b017204442f27f690cac7cc23a10fd1c837a856
Godspower <gpsimi01@gmail.com>
2024-11-01 10:32:01 +0100

    Update Testimonials.jsx

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -4,9 +4,12 @@ const Testimonials = () => {
   return (
     <section className="flex flex-col items-center">
       <div className="flex flex-col items-center ">
-        <h2  className="headings text-center max-w-3xl">
+        <h2 className="headings text-center max-w-3xl">
           What People are saying
         </h2>
+      </div>
+      <div className="">
+
       </div>
     </section>
   )
